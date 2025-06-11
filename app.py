12154b9f668f5935c80ea617ea136c0316bb67be
fachry26 <fachryanbia@gmail.com>
2025-06-11 09:53:04 +0700

    app

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ def download_file(filename):
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=6967)
